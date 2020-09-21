8eda4d7a5751934bd0b6e5d1d87069f0c3aa17c9
Ynda Jas <yndajas@gmail.com>
2020-09-21 15:16:07 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -8,4 +8,8 @@ class MusicImporter
   def files
     Dir.entries(path).select {|file| file.end_with?(".mp3")}
   end
+  
+  def import
+    self.files.each{|song|song.create_from_filename}
+  end
 end
